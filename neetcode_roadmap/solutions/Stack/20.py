ea5a82a06df15000196f5f07eb69565b306de784
slavatokmakov <ytokmakoff@gmail.com>
2025-07-16 08:15:54 +0300

    20

---
+++ neetcode_roadmap/solutions/Stack/20.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == "(":
+                stack.append(")")
+            elif c == "[":
+                stack.append("]")
+            elif c == "{":
+                stack.append("}")
+            elif not stack or stack.pop() != c:
+                return False
+        return not stack
